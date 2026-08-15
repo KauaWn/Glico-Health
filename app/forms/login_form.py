f7e53d22eefdbd50aecee3ea09ce92d668451d27
Anna Torres <anna.torres@escolar.ifrn.edu.br>
2026-08-15 09:11:47 -0300

    colocando em rotas diferentes

---
+++ app/forms/login_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm (FlaskForm):
+    username = StringField('Usuario', validators=[DataRequired()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    rm = BooleanField("Permanecer conectado")
+    submit = SubmitField('Entrar')
--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,34 @@
 from app import app
 from flask import render_template, redirect, flash, url_for
+
 from app.forms.cadastro_form import CadastroForm
 from app.controllers.UsuarioController import UserController
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def inicio():
+    return render_template("index.html")
+
+
+@app.route("/cadastro", methods=["GET", "POST"])
+def cadastro():
     form = CadastroForm()
-    mostrar_cadastro = False
 
     if form.validate_on_submit():
         if UserController.cadastro(form):
             flash("Cadastro efetuado com sucesso!")
             return redirect(url_for("home"))
+
         flash("Erro nas credenciais.")
-        mostrar_cadastro = True
-    elif form.is_submitted():
-        mostrar_cadastro = True
 
-    return render_template("index.html", form=form, mostrar_cadastro=mostrar_cadastro)
+    return render_template("cadastro.html", form=form)
 
-@app.route("/base")
-def base():
-    return render_template("base.html")
 
-@app.route("/home")
-def home():
-    return render_template("inicio.html")
+# @app.route("/home")
+# def home():
+#     return render_template("home.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
