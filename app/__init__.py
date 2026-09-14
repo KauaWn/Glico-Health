fb69246049f9400d819c1aa4c77a3ef491590321
Kauã Angelo <angelo.kaua@escolar.ifrn.edu.br>
2026-09-14 10:25:10 -0300

    automatização da criação do banco

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,30 @@ from flask_wtf import CSRFProtect
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import pymysql
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 csrf = CSRFProtect(app)
+
+
+def criar_banco():
+    conexao = pymysql.connect(
+        host=Config.DB_HOST,
+        user=Config.DB_USER,
+        password=Config.DB_PASSWORD
+    )
+
+    cursor = conexao.cursor()
+
+    cursor.execute("CREATE DATABASE IF NOT EXISTS glicohealthbd")
+
+    cursor.close()
+    conexao.close()
+
+criar_banco()
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import secrets
 #from urllib.parse import quote_plus
-
 class Config:
     SECRET_KEY = secrets.token_hex(16) 
+    DB_HOST = "localhost"
+    DB_USER = "root"
+    DB_PASSWORD = "labinfo"
+    DB_NAME = "glicohealthbd"
     #senha_com_arroba = "" 
     #senha_seg = quote_plus(senha_com_arroba)
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://root:kaua2007@localhost/glicohealthbd'
+    SQLALCHEMY_DATABASE_URI = (f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
